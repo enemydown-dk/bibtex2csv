38d3b04b88c608c94973ee71505066b23e794017
Asger Juel Hansen <enemydown-dk@users.noreply.github.com>
2017-06-26 17:53:13 +0200

    Added HOWTO to README.md + small changes to main program.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # bibtex2csv
-Python script that converts BibTeX entries to CSV
+Python script that converts BibTeX entries to CSV.
+
+Howto convert & add fields to extract:
+Convert single BibTex file (.bib) with the command ./bibtex2csv.py < [.bib file] > [output file]
+
+Convert multiple BibTex files (.bib) with the command cat *.bib | ./bibtex2csv > [output file]
+
+Add extra fields to extract by adding fields to the below code in main program.
+for entry in entries:
+    doi = entry["DOI"]
+    title = entry["Title"]
+    oa = entry["OA"]
+    -> new_variable = entry["name_from_bibtex_file"]
+
+    add \t{} to line for as many new fields as you add
+       -> print("{}\t{}\t{}".format(doi, title, oa))
---
+++ bibtex2csv.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+"""
+Python script that converts BibTeX
+entries to CSV.
+Input is via standard input.
+Output is via standard output.
+"""
+
+from re import match
+from re import search
+from re import findall
+from sys import stdin
+
+entries = []
+entry = {}
+
+for line in stdin:
+    if match('^@', line.strip()):
+        if entry != {}:
+            entries.append(entry)
+            entry = {}
+    elif match('url', line.strip()):
+        #value, = findall('\{(\S+)\}', line)
+        value, = findall("\\s+", line)
+        entry["url"] = value
+    elif search('=', line.strip()):
+        key, value = [v.strip(" {},\n") for v in line.split("=", 1)]
+        entry[key] = value
+
+for entry in entries:
+    doi = entry["DOI"]
+    title = entry["Title"]
+    oa = entry["OA"]
+    affiliation = ["Affiliation"]
+    print("{}\t{}\t{}\t{}".format(doi, title, oa, affiliation))
